f77d4ea66c6e054d31ab69cfd30c7d2a105dbe44
yurialb <yuri.albuquerque@outlook.com>
2021-11-08 17:05:56 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,6 +54,16 @@
   })
 
   /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
    * Scrool with ofset on links with a class name .scrollto
    */
   on('click', '#navbar .nav-link', function(e) {
@@ -243,5 +253,5 @@
       clickable: true
     }
   });
-
+  
 })()
